2cc7d5801c9ae9db1ea77e80f0fc025ff7f6ff74
3dogCarol <carollawrence67@yahoo.com>
2013-08-05 13:26:37 +0000

    another test

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
-var buf=fs.readFileSync("index.html");
+//var buf=fs.readFileSync("index.html");
 
 //console.log(buf);
 //console.log(buf.toString());
