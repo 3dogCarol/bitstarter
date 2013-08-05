9fb22b9d74da990f876d53f963812178011b7cf9
3dogCarol <carollawrence67@yahoo.com>
2013-08-05 12:28:28 +0000

    testing

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
-
 var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 var buf=fs.readFileSync("index.html");
-console.log(buf.toString());
+
+//console.log(buf);
+//console.log(buf.toString());
 
 app.get('/', function(request, response) {
   response.send('Hello World Too!');
 });
-app.get('/', function(request, response) {
-  response.send(buf.toString())
-});
+//app.get('/', function(request, response) {
+  //response.send(buf.toString())
+//});
 
 var port = process.env.PORT || 5000;
 
