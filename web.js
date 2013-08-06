c46934d4aa4a02b4f85e2e22f9c2801b8c03976f
3dogCarol <carollawrence67@yahoo.com>
2013-08-06 13:21:56 +0000

    changed but to buf

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.use(express.logger());
 var buf=fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(but.toString());
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
