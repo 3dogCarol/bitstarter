0ca045da6dd12c3ed9a0701ce733607106649a47
3dogCarol <carollawrence67@yahoo.com>
2013-08-06 01:01:08 +0000

    another try

--- web.js
+++ web.js
@@ -1,21 +1,13 @@
-//var fs = require('fs');
+var fx - require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
-//var buf=fs.readFileSync("index.html");
-
-//console.log(buf);
-//console.log(buf.toString());
 
 app.get('/', function(request, response) {
-  response.send('Hello World Too!');
+  response.send('Hello World!');
 });
-//app.get('/', function(request, response) {
-  //response.send(buf.toString())
-//});
 
 var port = process.env.PORT || 5000;
-
 app.listen(port, function() {
-     console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
